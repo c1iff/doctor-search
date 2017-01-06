d4a696012ab479f2db5683d02206a7f40eeb275c
Chris Clifford <cclifford82@gmail.com>
2017-01-06 10:33:09 -0800

    add callback function for api call

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,6 +1,17 @@
 var DoctorSearch = require('./../js/doctor-search.js').doctorModule;
 
+var getDoctors = function(results) {
+  results.data.forEach(function(each) {
+    console.log(each);
+    $('#results').append('<li>Dr.' + each.profile.last_name + '</li>')
+  })
+}
+
 $(function(){
-  var newSearch = new DoctorSearch()
-  newSearch.getDoctors('broken leg')
+  $('#search').click(function() {
+    console.log('submitted');
+    var newSearch = new DoctorSearch();
+    searchValue = $('#search-value').val();
+    newSearch.getDoctors(searchValue, getDoctors);
+  })
 });
--- js/doctor-search.js
+++ js/doctor-search.js
@@ -3,10 +3,10 @@ var apiKey = require('./../.env').apiKey;
 function DoctorSearch(){
 }
 
-DoctorSearch.prototype.getDoctors = function(medicalIssue) {
+DoctorSearch.prototype.getDoctors = function(medicalIssue, displayFunction) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-      console.log(result);
+   .then(function(results) {
+      displayFunction(results)  
     })
    .fail(function(error){
       console.log("fail");

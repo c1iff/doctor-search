01983e9419762688e26df9e0b9ac27bda1f635bd
Chris Clifford <cclifford82@gmail.com>
2017-01-06 12:21:36 -0800

    add code to display doctor results with html tags

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -3,15 +3,37 @@ var DoctorSearch = require('./../js/doctor-search.js').doctorModule;
 var getDoctors = function(results) {
   results.data.forEach(function(each) {
     console.log(each);
-    $('#results').append('<li>Dr.' + each.profile.last_name + '</li>')
-  })
-}
+    var distanceString = "";
+    if (parseInt(each.practices[0].distance) <= 1) {
+      distanceString = parseInt(each.practices[0].distance) + ' Mile Away</a>';
+    } else {
+      distanceString = parseInt(each.practices[0].distance) + ' Miles Away</a>';
+    }
+    $('#results').append('<div class="card horizontal">' +
+                            '<div class="valign-wrapper">' +
+                              '<img id="profile-pic" src="' + each.profile.image_url +'" class="circle responsive-img">' +
+                            '</div>' +
+                            '<div class="card-stacked">' +
+                              '<div class="card-content">' +
+                                '<h3>'+ each.profile.last_name + ', ' + each.profile.first_name + ' ' + each.profile.middle_name.charAt(0) + ', ' + each.profile.title + '</h3>'+
+                                '<h4>' + each.specialties[0].name + '</h4><hr>' +
+                                '<p>' + each.practices[0].visit_address.street + '</p>' +
+                                '<p>' + each.practices[0].visit_address.city + ', ' + each.practices[0].visit_address.state + '</p>' +
+                                '<p>' + 'Physician ID: ' + each.npi +'</p>' +
+                                '<a href="#">' + each.practices[0].phones[0].number + '</a>' +
+                              '</div>' +
+                              '<div class="card-action">' +
+                                '<a href="http://maps.google.com/?ll=' + each.practices[0].visit_address.lat + ',' + each.practices[0].visit_address.lon + '" target="_blank">' + distanceString +
+                              '</div>' +
+                            '</div>' +
+                          '</div>');
+    });
+  };
 
 $(function(){
   $('#search').click(function() {
-    console.log('submitted');
     var newSearch = new DoctorSearch();
     searchValue = $('#search-value').val();
     newSearch.getDoctors(searchValue, getDoctors);
-  })
+  });
 });
--- js/doctor-search.js
+++ js/doctor-search.js
@@ -6,7 +6,7 @@ function DoctorSearch(){
 DoctorSearch.prototype.getDoctors = function(medicalIssue, displayFunction) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(results) {
-      displayFunction(results)  
+      displayFunction(results);  
     })
    .fail(function(error){
       console.log("fail");
